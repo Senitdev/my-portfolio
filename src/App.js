5962733494f138fd4e1c820b89b9f62026759dc2
Senitdev <pape.toure@senitdev.com>
2025-01-13 13:40:41 +0000

    Réglage du menu CM du 13.01.2025

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import logo from './logo.svg';
-
+import About from './composant/Abouts';
+import Blog from './composant/Blog';
+import Header from './composant/Header';
+import Layout from './composant/Layout';
+import PortFolio from './composant/PortFolio';
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+  children:[
+  {
+    path:"/about",
+    element:<About/>
+  },
+  {
+   path:"/blog",
+   element:<Blog/>
+  },
+  {
+    path:"/portfolio",
+    element:<PortFolio/>
+  }
+]}
+]);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Bonjour mes amies</p>
-        <p>Je test le déploiement  par Vercel</p>
-        <p>Formulaire de saisie</p>
-        <label>Prenom</label>
-        <input type='text' placeholder='saisir ton prenom'></input>
-        <label>Nom</label>
-        <input type='text' placeholder='Saisir ton nom'></input>
-        <button>Envoyer</button>
-      </header>
-    </div>
-  );
+  return <div>
+   <Header/>
+    <RouterProvider router={router}></RouterProvider>
+  </div>
 }
 
 export default App;
---
+++ src/composant/Header.jsx
@@ -0,0 +1,34 @@
+import '../../src/App.css';
+
+export default function Header(){
+    const menu=[
+        {
+          id:1,
+          libelle:"About",
+          link:"/about"
+        },
+        {
+          id:2,
+          libelle:"Blog",
+          link:"/blog"
+        },
+        {
+          id:3,
+          libelle:"PortFolio",
+          link:"/portfolio"
+        },
+    ]
+    return<>
+   <div className="App">
+     <div className='menu'>
+    <ul style={{ display:"flex",flexDirection:"row",height:"30px",color:"black", justifyContent:"space-between", width:"25%"}}>
+      {
+  menu.map((elt)=>(
+    <li key={elt.id}><a href={elt.link}>{elt.libelle}</a></li>
+  ))
+      }
+    </ul>
+    </div>
+    </div>
+    </>
+}
