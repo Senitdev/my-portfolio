79df97e14618973f8d30f4c7d87b5b56aa970e0d
Senitdev <pape.toure@senitdev.com>
2025-01-13 22:05:12 +0000

    Formulaire de contact

--- src/composant/Form.jsx
+++ src/composant/Form.jsx
@@ -1,12 +1,15 @@
+import "./form.css";
 export default function FormContact(){
     return<>
     <p>Veuillez nous contacter</p>
+    <form>
     <label>Subject</label>
     <input type="text" placeholder="Subject"></input>
     <label>Email</label>
     <input type="email" placeholder="Email"></input>
     <label>Message</label>
-    <textarea cols={5} rows={3}></textarea>
+    <textarea></textarea>
     <button className="btn_envoyer">Envoyer</button>
+    </form>
     </>
 }
